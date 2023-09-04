ed8c250378f1810eccde1ad4f11dc2244381a9ff
Mariano Garmendia <marianogarmendia77@gmail.com>
2023-09-04 19:04:46 -0300

    funcion eliminar Li y agregar Li

--- index.js
+++ index.js
@@ -25,6 +25,31 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+const eliminarLi = () => {
+  const listaEl = document.querySelector(".lista");
+  const liEl = listaEl.querySelectorAll("li");
+  liEl.forEach((li) => {
+    li.remove();
+  });
+};
 
+const agregarClases = (clase) => {
+  cosasQueAprendimos.forEach((cosa) => {
+    cosa.class = clase;
+  });
+};
+const agregarLi = () => {
+  cosasQueAprendimos.forEach((cosa) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = cosa.tema;
+    liEl.classList.add(cosa.class);
+    const listaEl = document.querySelector(".lista");
+    listaEl.append(liEl);
+  });
+};
+
+function main() {}
+eliminarLi();
+agregarClases("special");
+agregarLi();
 main();
